ea2a106b923c2d1559e39b353832daf6f757a331
Sangmin Lee <sanglee2@c9r3s12.42seoul.kr>
2023-05-30 22:40:08 +0900

    starting norm

--- deq_a_loc.c
+++ deq_a_loc.c
@@ -6,7 +6,7 @@
 /*   By: sanglee2 <sanglee2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/28 20:19:28 by sanglee2          #+#    #+#             */
-/*   Updated: 2023/05/30 22:18:58 by sanglee2         ###   ########.fr       */
+/*   Updated: 2023/05/30 22:32:22 by sanglee2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,16 +17,16 @@ static	int	get_a_loc_bigger(t_deq* deq_a, t_node* temp)
 {
 	int deq_a_loc;
 	int cur_loc;
-	int min;
+	int min_a;
 			
 	cur_loc = 0;
-	min = INT_MAX;
+	min_a = INT_MAX;
 	temp = deq_a->a_top;
 	while(temp)
 	{
-		if (temp->index < min)
+		if (temp->index < min_a)
 		{
-			min = temp->index;
+			min_a = temp->index;
 			deq_a_loc = cur_loc;
 		}
 		temp = temp->next;
@@ -42,38 +42,26 @@ int get_a_loc(t_deq* deq_a, int b_content)
 	int min;
 	t_node* temp;
 
-	deq_a_loc = 0;
 	cur_loc = 0;
 	min = INT_MAX;
 	temp = deq_a->a_top;
-
 	while (temp)
 	{
-		if (b_content < temp->index)
+		if (b_content < temp->index && min > temp->index - b_content)
 		{
-			if (min > temp->index - b_content)
-			{
-				min = temp->index - b_content;
-				deq_a_loc = cur_loc;
-			}
+			min = temp->index - b_content;
+			deq_a_loc = cur_loc;
 		}
 		temp = temp->next;
 		cur_loc++;
 	}
 	if (min == INT_MAX)
-	{
 		deq_a_loc = get_a_loc_bigger(deq_a, temp);
-	}
 	//deq_a->a_size = get_deq_a_size(deq_a);
 	if (deq_a_loc > deq_a->a_size / 2)
 		deq_a_loc = (deq_a->a_size - deq_a_loc) * -1;
 	return (deq_a_loc);
 }
-
-
-
-
-
 
 // int get_a_loc(t_deq* deq_a, int b_content)
 // {
